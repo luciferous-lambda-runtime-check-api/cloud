527e7038bb0508d707f7c9871dc2c45132620fd3
sinofseven <em.s.00001@gmail.com>
2023-09-13 11:04:37 +0900

    fix get python version

--- src/handlers/get_version/python.py
+++ src/handlers/get_version/python.py
@@ -6,6 +6,6 @@ def handler(event, context):
     return "&".join(
         [
             f"AWS_EXECUTION_ENV={os.getenv('AWS_EXECUTION_ENV')}",
-            f"VERSION=${sys.version}",
+            f"VERSION={sys.version}",
         ]
     )

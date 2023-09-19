ab9fec8032d8751cacf4ddfb327c34b5b7bd7f09
sinofseven <em.s.00001@gmail.com>
2023-09-20 04:11:55 +0900

    impl

---
+++ src/handlers/create_invalidation.py
@@ -0,0 +1,26 @@
+from utils.create_client import create_client
+from utils.create_logger import create_logger
+from utils.decorate_handler import decorate_handler
+from utils.load_environment import load_environment
+
+logger = create_logger(__name__)
+
+
+@decorate_handler(logger)
+def handler(_event, _context, cloudfront_client=create_client("cloudfront")):
+    env = load_environment()
+    option = {
+        "DistributionId": env.distribution_id,
+        "InvalidationBatch": {"Paths": {"Quantity": 1, "Items": ["/*"]}},
+    }
+    logger.info(
+        "create_invalidation",
+        extra={
+            "command_info": {
+                "service": "cloudfront",
+                "api": "create_invalidation",
+                "option": option,
+            }
+        },
+    )
+    cloudfront_client.create_invalidation(**option)

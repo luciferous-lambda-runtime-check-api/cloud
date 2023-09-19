a4e9ab63312bf909a34330d59c9556260bd402a7
sinofseven <em.s.00001@gmail.com>
2023-09-20 04:32:46 +0900

    fix validation

--- src/handlers/create_invalidation.py
+++ src/handlers/create_invalidation.py
@@ -2,6 +2,7 @@ from utils.create_client import create_client
 from utils.create_logger import create_logger
 from utils.decorate_handler import decorate_handler
 from utils.load_environment import load_environment
+from datetime import datetime, timezone
 
 logger = create_logger(__name__)
 
@@ -11,7 +12,10 @@ def handler(_event, _context, cloudfront_client=create_client("cloudfront")):
     env = load_environment()
     option = {
         "DistributionId": env.distribution_id,
-        "InvalidationBatch": {"Paths": {"Quantity": 1, "Items": ["/*"]}},
+        "InvalidationBatch": {
+            "Paths": {"Quantity": 1, "Items": ["/*"]},
+            "CallerReference": str(datetime.now(timezone.utc).timestamp()),
+        },
     }
     logger.info(
         "create_invalidation",
